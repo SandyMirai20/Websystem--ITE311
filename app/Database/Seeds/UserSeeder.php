505006ca44993fc372ea0145a2b00e1e2241222c
SandyMirai20 <sandymirai20@gmail.com>
2025-08-16 10:10:25 +0800

    Added database migrations and seeders

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Crisandy Gomez',
+                'email' => 'GomezCrisandy@gmail.com',
+                'password' => password_hash('admin123', PASSWORD_DEFAULT),
+                'role' => 'admin',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Jerald Maca',
+                'email' => 'JeraldMaca@gmail.com',
+                'password' => password_hash('2311600073', PASSWORD_DEFAULT),
+                'role' => 'student',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Jim Jamero',
+                'email' => 'Jamero@gail.com',
+                'password' => password_hash('prof123', PASSWORD_DEFAULT),
+                'role' => 'instructor',
+                'created_at' => date('Y-m-d H:i:s')
+            ]
+        ];
+        // Insert all data in one go
+        $this->db->table('users')->insertBatch($data);
+    }
+}
