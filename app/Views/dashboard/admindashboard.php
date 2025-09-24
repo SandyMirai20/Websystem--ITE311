d3f8062b394aaa90c74802416e8d562ab051611a
SandyMirai20 <sandymirai20@gmail.com>
2025-09-24 20:56:59 +0800

    Updated dashboard method in Auth controller with role-based data and authorization

--- app/Views/dashboard/admindashboard.php
+++ app/Views/dashboard/admindashboard.php
@@ -32,7 +32,6 @@
     </style>
 </head>
 <body class="bg-light">
-    <!-- Navigation bar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
         <div class="container">
             <a class="navbar-brand" href="<?= base_url('/') ?>">
